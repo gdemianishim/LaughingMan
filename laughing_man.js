a8e088bc75b5b33026fb13362d6c403aad14aef8
gdemianishim <georgedemian84@gmail.com>
2025-12-02 23:34:19 -0500

    fixed code, should fully function now

--- laughing_man.js
+++ laughing_man.js
@@ -1,5 +1,5 @@
-const SQUARE_COUNT = 3;
-const SPEED = 3;
+const SQUARE_COUNT = 30;
+const SPEED = 10;
 const TIMER_SPEED = 100;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,89 +8,58 @@ document.addEventListener('DOMContentLoaded', () => {
     squareElement.addEventListener("click", () => {
         alert("OMG YOU CAUGHT ME!");
     });
+    // change square color to random color on hover
+    squareElement.addEventListener('mouseover', () => {
+        squareElement.style.backgroundColor = getRandomColor();
+    });
 
     let box = document.querySelector("#box");
 
     for (let i = 0; i < SQUARE_COUNT; i++) {
-        //create the square
+        // create the square
         let square = document.createElement('img');
         square.src = 'laughing_man.jpg';
-        square.alt = 'Catch the Laughing Man!';
+        square.alt = 'Catch the laughing man!';
         square.className = 'square';
+        // change image on hover, revert on mouseout
+        square.addEventListener('mouseover', () => {
+            square.src = 'orochimaru_50x50.jpg';
+        });
+        square.addEventListener('mouseout', () => {
+            square.src = 'laughing_man.jpg';
+        });
         box.appendChild(square);
-        //add it to the screen   
+        // add it to the screen
     }
+
     let allSquares = box.children;
     let squareArray = Array.from(allSquares);
-    squareArray.forEach(function (element, index) {
+    squareArray.forEach((element) => {
         var dx = SPEED * (Math.random() * 2 - 1);
         var dy = SPEED * (Math.random() * 2 - 1);
 
-            
-    if (element.tagName.toLowerCase() === "img") {
-        element.addEventListener("mouseover", function () {
-            element.src = "orochimaru_50x50.jpg"; 
-        });
-
-        element.addEventListener("mouseout", function () {
-            element.src = "laughing_man.jpg";
-        });
-    }
-
-
-
         setInterval(() => {
-            let currentLeft = parseInt(element.style.left) || 225;
-            let currentTop = parseInt(element.style.Top) || 175;
+            let currentLeft = parseFloat(element.style.left) || 225;
+            let currentTop = parseFloat(element.style.top) || 175;
+
             if (currentLeft >= 450 || currentLeft <= 0) {
-
-
                 dx *= -1;
-                element.style.borderColor = getColor();
-                element.style.backgroundColor = getColor();
+                element.style.borderColor = getRandomColor();
             }
-
             if (currentTop >= 350 || currentTop <= 0) {
                 dy *= -1;
-                element.style.borderColor = getColor();
-                element.style.backgroundColor = getColor();
+                element.style.borderColor = getRandomColor();
             }
 
-            
-
-            // {
-            //     dx*=-1
-            //     element.style.borderColor = getRandomColor();
-
-            // }
-
-            //     if (currentTop >=350 || currentTop <=0)
-            // {
-            //     dy*=-1
-            //      element.style.borderColor = getRandomColor();
-            // }        
-
-            element.style.left = (currentLeft + dx) + "px"
-            element.style.Top = (currentTop + dx) + "px"
-
+            element.style.left = (currentLeft + dx) + "px";
+            element.style.top = (currentTop + dy) + "px";
         }, TIMER_SPEED);
-
     });
-
 });
 
 function getRandomColor() {
-
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},rgb(${g},rgb(${b})`;
-
-    // var r = parsInt (amath.random()*256);
-    // var g = parsInt (amath.random()*256);
-    // var b = parsInt (amath.random()*256);
-    // let color = `rgb(${r},rgb(${g},rgb(${b})`;
-    // return color;
+    let r = parseInt(Math.random() * 256);
+    let g = parseInt(Math.random() * 256);
+    let b = parseInt(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
 }
-
-//homework 
